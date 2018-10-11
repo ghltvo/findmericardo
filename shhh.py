57c52433e56080c91e8b69d720ede407fe46c8d7
ghltvo <43759604+ghltvo@users.noreply.github.com>
2018-10-11 13:45:02 -0500

    Create shhh.py

---
+++ shhh.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import boto3
+
+username = "richy.lafosse"
+password = "12345"
+
+aws_access_key_id = "ASIA1234569082"
+aws_secret_access_key = "yougotobekiddingme"
+
+
+client = boto2.client('ec2')
